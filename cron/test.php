da80856e6c2166499ca8c6a43665996864f720b4
Brayan <bbmo.181299@gmail.com>
2019-03-06 16:10:51 -0400

    Added order logic

--- cron/test.php
+++ cron/test.php
@@ -1,6 +1,7 @@
 <?php
 
 ini_set('max_execution_time', 600000);
+//$con =mysqli_connect('localhost','root','','cryptocompare') or die(mysqli_error());
 $con =mysqli_connect('localhost','cryptouser','crypto123','cryptocompare') or die(mysqli_error());
 
     $final_data = array();
@@ -10,11 +11,11 @@ $decoded = json_decode($api_core_data);
 
 
 foreach ($decoded->Data as $key => $value) {
-
+		//var_dump($value->NAME);
 		$list = array(1182, 3808, 7605);
 
-		if (in_array($value->Id, $list))
-		{
+		//if (in_array($value->Id, $list))
+		//{
 		  	//
 			$coin_name = $value->Name;
 			$chart_image = "https://images.cryptocompare.com/sparkchart/".$value->Name."/USD/latest.png?ts=".microtime(true);
@@ -26,7 +27,7 @@ foreach ($decoded->Data as $key => $value) {
 			$d_sql = "select * from live_data where id=".$value->Id;
 			$check=mysqli_query($con,$d_sql);
 			$count=mysqli_num_rows($check);
-
+			echo $coin_details->RAW->$coin_name->USD->MKTCAP;
 			if($count==0){
 
 				$sql="INSERT INTO live_data(id, name, price, percent_change_24h, volume_24h, market_cap, image_url, chart_image) VALUES($value->Id,
@@ -34,7 +35,7 @@ foreach ($decoded->Data as $key => $value) {
 				'".$coin_details->DISPLAY->$coin_name->USD->PRICE."',
 				'".$coin_details->DISPLAY->$coin_name->USD->CHANGEPCT24HOUR."',
 				'".$coin_details->DISPLAY->$coin_name->USD->VOLUME24HOUR."',
-				'".$coin_details->DISPLAY->$coin_name->USD->MKTCAP."',
+				'".$coin_details->RAW->$coin_name->USD->MKTCAP."',
 				'".$coin_image."',
 				'".$chart_image."')";
 				$insert=mysqli_query($con,$sql) or die(mysqli_error());
@@ -44,7 +45,7 @@ foreach ($decoded->Data as $key => $value) {
 						price='".$coin_details->DISPLAY->$coin_name->USD->PRICE."', 
 						percent_change_24h='".$coin_details->DISPLAY->$coin_name->USD->CHANGEPCT24HOUR."', 
 						volume_24h='".$coin_details->DISPLAY->$coin_name->USD->VOLUME24HOUR."', 
-						market_cap='".$coin_details->DISPLAY->$coin_name->USD->MKTCAP."',
+						market_cap='".$coin_details->RAW->$coin_name->USD->MKTCAP."',
 						image_url='".$coin_image."', 
 						chart_image='".$chart_image."'
 						 where id= '".$value->Id."'";
@@ -52,7 +53,7 @@ foreach ($decoded->Data as $key => $value) {
 						
 				$update=mysqli_query($con,$sql) or die(mysqli_error());
 			}
-		}
+		//}
 }	
 			
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,6 +26,7 @@ img.single-img-pro {
 img.single-img-pro {
         padding-top: 50px;
 }
+
 @media only screen and (max-width: 766px) {
 .hide-add-div {
     display: none !important;
@@ -170,7 +171,10 @@ var api_url1 = window.location.protocol + "//" + window.location.host + "/";
 
 var start = '0';
 var g_data;
-setInterval(function() {
+let saveOrder = '';
+let orderAscDesc = [false, false, false, false, false];
+setInterval( ajaxFunction , 3000);
+function ajaxFunction() {
     // Ajax starts
     $.ajax({
         type: "GET",
@@ -178,10 +182,75 @@ setInterval(function() {
         dataType: 'json',
         success: function(data) {
             start = data.start;
-            console.log(data.data);
-            //alert(data);
-            var temp = '<div class="table-responsive"><table id="example1" class="table"> <thead class="flip-content"> <tr> <th>Cryptocurrency</th> <th>Price</th> <th class="numeric">24h % change</th> <th class="numeric">Volume</th> <th class="numeric">Market cap</th> <th class="numeric">24h performence</th> </tr></thead> <tbody>';
-            //temp += g_data;
+            data.data.forEach(function(element){
+                element.price = element.price.replace('$', '');
+                element.price = element.price.replace(',', '');
+                element.volume_24h = element.volume_24h.replace(/,/g, '');
+                element.market_cap = parseFloat(element.market_cap);
+                element.market_cap = element.market_cap.toFixed(2);
+            });
+            //console.log(data.data);
+            switch(saveOrder){
+                case "name": {
+                    if(orderAscDesc[0]) data.data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+                    else data.data.sort((a, b) => (a.name < b.name) ? 1 : -1);
+                    break;
+                }
+                case "price": {
+                    if(orderAscDesc[1]){ 
+                        data.data.sort((a, b) => {
+                            return (parseFloat(a.price) > parseFloat(b.price) ? 1 : -1);
+                        });
+                    }
+                    else{
+                        data.data.sort((a, b) => {
+                            return (parseFloat(a.price) < parseFloat(b.price) ? 1 : -1);
+                        });
+                    }
+                    break;
+                }
+                case "percent_change_24h": {
+                    if(orderAscDesc[2]){ 
+                        data.data.sort((a, b) => {
+                            return data.data.sort((a, b) => (parseFloat(a.percent_change_24h) > parseFloat(b.percent_change_24h)) ? 1 : -1);
+                        });
+                    }
+                    else{
+                        data.data.sort((a, b) => {
+                            return data.data.sort((a, b) => (parseFloat(a.percent_change_24h) < parseFloat(b.percent_change_24h)) ? 1 : -1);
+                        }); 
+                    }
+                    break;
+                }
+                case "volume_24h" :{
+                    if(orderAscDesc[3]){
+                        data.data.sort((a, b) => {
+                            return ((parseFloat(a.volume_24h) > parseFloat(b.volume_24h)) ? 1 : -1);
+                        });
+                    }
+                    else{
+                        data.data.sort((a, b) => {
+                            return ((parseFloat(a.volume_24h) < parseFloat(b.volume_24h)) ? 1 : -1);
+                        });
+                    }
+                    break;
+                }
+                case "market_cap" :{
+                    if(orderAscDesc[4]){
+                        data.data.sort((a, b) => {
+                            return ((parseFloat(a.market_cap) > parseFloat(b.market_cap)) ? 1 : -1);
+                        });
+                    } 
+                    else{
+                        data.data.sort((a, b) => {
+                            return ((parseFloat(a.market_cap) < parseFloat(b.market_cap)) ? 1 : -1);
+                        });
+                    }
+                    break;
+                }
+            }
+            
+            var temp = '<div class="table-responsive"><table id="example1" class="table"> <thead class="flip-content"> <tr> <th onclick=orderBy("name") style="cursor: pointer">Cryptocurrency</th> <th onclick=orderBy("price") style="cursor: pointer">Price</th> <th onclick=orderBy("percent_change_24h") style="cursor: pointer" class="numeric">24h % change</th> <th class="numeric" onclick=orderBy("volume_24h") style="cursor: pointer">Volume</th> <th class="numeric" onclick=orderBy("market_cap") style="cursor: pointer">Market cap</th> <th class="numeric">24h performence</th> </tr></thead> <tbody>';
             data.data.forEach(function(element) {
 
                 var o_graph = $("#G"+element.id).val();
@@ -192,27 +261,27 @@ setInterval(function() {
                 var price_old = $("#" + element.id).html();
                 //price_old = parseFloat(price_old);
                 price_new = parseFloat(element.price);
+                
+                price_old = price_old.replace('$ ', '');
 
-                price_old = price_old.replace('$', '');
-                element.price = element.price.replace('$', '');
-                // alert(price_old);
-                // alert(element.price);
+                element.price = element.price.replace(' ', '');
+
                 var t_url = '"' + api_url1 + element.id + '?name=' + element.name + '"';
                 if (element.percent_change_24h < 0) {
                     var percent_back = "width: 55px;max-width: 55px;background-color:#ed5565!important;color: #fff;border-radius: 30px; font-size: 0.8em;padding:5px 5px;display: inline-block;text-align:center;padding-bottom:3px;";
                 } else {
                     var percent_back = "width: 55px;max-width: 55px;background-color:#57bd0f!important;color: #fff;border-radius: 30px; font-size: 0.8em;padding:5px 5px;display: inline-block;text-align:center;padding-bottom:3px;";
                 }
-                //element.name = '"'+element.name+'"';
+
                 if (price_old < element.price) {
                     console.log("Value increased");
 
-                    temp += '<tr onclick="test(' + element.id + ');" style="background-color: #8acc8a;cursor: pointer;"> <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;'+element.name+'</td><td id="' + element.id + '">' + element.price + '</td><td class="numeric"><div style="' + percent_back + '"><span>' + element.percent_change_24h + '</span></div></td><td class="numeric">' + element.volume_24h + '</td><td class="numeric" style="color:rgb(66, 139, 202);">' + element.market_cap + '</td><td class="numeric"> <img height="35" style="border: 1px solid #eee;" width="100" src="' + o_graph + '"> </td></tr>';
+                    temp += '<tr onclick="test(' + element.id + ');" style="background-color: #8acc8a;cursor: pointer;"> <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;'+element.name+'</td><td id="' + element.id + '">$ ' + element.price + '</td><td class="numeric"><div style="' + percent_back + '"><span>' + element.percent_change_24h + '</span></div></td><td class="numeric">' + element.volume_24h + '</td><td class="numeric" style="color:rgb(66, 139, 202);">$ ' + element.market_cap + '</td><td class="numeric"> <img height="35" style="border: 1px solid #eee;" width="100" src="' + o_graph + '"> </td></tr>';
                 } else if (price_old > element.price) {
                     console.log("Value decreased");
-                    temp += '<tr onclick="test(' + element.id + ');" style="background-color: #e46565;cursor: pointer;"> <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;'+element.name+'</td><td id="' + element.id + '">' + element.price + '</td><td class="numeric"><div style="' + percent_back + '"><span>' + element.percent_change_24h + '</span></div></td><td class="numeric">' + element.volume_24h + '</td><td class="numeric" style="color:rgb(66, 139, 202);">' + element.market_cap + '</td><td class="numeric"> <img height="35" style="border: 1px solid #eee;" width="100" src="' + o_graph + '"> </td></tr>';
+                    temp += '<tr onclick="test(' + element.id + ');" style="background-color: #e46565;cursor: pointer;"> <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;'+element.name+'</td><td id="' + element.id + '">$ ' + element.price + '</td><td class="numeric"><div style="' + percent_back + '"><span>' + element.percent_change_24h + '</span></div></td><td class="numeric">' + element.volume_24h + '</td><td class="numeric" style="color:rgb(66, 139, 202);">$ ' + element.market_cap + '</td><td class="numeric"> <img height="35" style="border: 1px solid #eee;" width="100" src="' + o_graph + '"> </td></tr>';
                 } else {
-                    temp += '<tr style="cursor: pointer;" onclick="test(' + element.id + ');" > <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;'+element.name+'</td><td id="' + element.id + '">' + element.price + '</td><td class="numeric"><div style="' + percent_back + '"><span>' + element.percent_change_24h + '</span></div></td><td class="numeric">' + element.volume_24h + '</td><td class="numeric" style="color:rgb(66, 139, 202);">' + element.market_cap + '</td><td class="numeric"> <img height="35" style="border: 1px solid #eee;" width="100" src="' + o_graph + '"> </td></tr>';
+                    temp += '<tr style="cursor: pointer;" onclick="test(' + element.id + ');" > <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;'+element.name+'</td><td id="' + element.id + '">$ ' + element.price + '</td><td class="numeric"><div style="' + percent_back + '"><span>' + element.percent_change_24h + '</span></div></td><td class="numeric">' + element.volume_24h + '</td><td class="numeric" style="color:rgb(66, 139, 202);">$ ' + element.market_cap + '</td><td class="numeric"> <img height="35" style="border: 1px solid #eee;" width="100" src="' + o_graph + '"> </td></tr>';
                 }
 
 
@@ -232,7 +301,7 @@ setInterval(function() {
                 if (i == 6) {
                     page_html += '<button class="btn pagination-btn">...</button><button class="btn pagination-btn" onclick="insertParam(' + i + ');">Next</button>';
                 }
-                //page_html += '<button class="btn" onclick="insertParam('+i+');">'+i+'</button>';
+
             }
 
             var last = data.recordsTotal / 25;
@@ -245,16 +314,56 @@ setInterval(function() {
         }
     });
     // end of ajax
+}
 
-}, 3000);
+/***********************************************/
+/*     Events on table  (Create by Brayan)     */
+/***********************************************/
+function orderBy(order){
+    //alert(order);
+    saveOrder = order;
+    switch(saveOrder){
+        case "name": {
+            if(orderAscDesc[0])  orderAscDesc[0]=false;
+            else orderAscDesc[0] = true;
+            break;
+        }
+        case "price": {
+            if(orderAscDesc[1])  orderAscDesc[1]=false;
+            else orderAscDesc[1] = true;
+            break;
+        }
+        case "percent_change_24h": {
+            if(orderAscDesc[2])  orderAscDesc[2]=false;
+            else orderAscDesc[2] = true;
+            break;
+        }
+        case "volume_24h" :{
+            if(orderAscDesc[3])  orderAscDesc[3]=false;
+            else orderAscDesc[3] = true;
+            break;
+        }
+        case "market_cap" :{
+            if(orderAscDesc[4])  orderAscDesc[4]=false;
+            else orderAscDesc[4] = true;
+            break;
+        }
+    }
+    ajaxFunction();
+}
+
+/***********************************************/
+/***********************************************/
+
 
 setInterval(function() {
     var cron_url = "https://market.fasbit.com/cron/test.php";
+    //var cron_url = "http://cryptocompare.local/cron/test.php";
     $.ajax({
         type: "GET",
         url: cron_url,
         success: function(data) {
-
+            //console.log(data);
         }
     });
 }, 3000);
@@ -272,7 +381,9 @@ $.ajax({
 
         var temp = '<table id="example1" class="table"> <thead class="flip-content"> <tr> <th>Cryptocurrency</th> <th>Price</th> <th class="numeric">24h % change</th> <th class="numeric">Volume</th> <th class="numeric">Market cap</th> <th class="numeric">24h performence</th> </tr></thead> <tbody>';
         data.data.forEach(function(element) {
-
+            element.price = element.price.replace(',', '');
+            element.volume_24h = element.volume_24h.replace(/,/g, '');
+            
             var t_tt = '<input type="hidden" id="R' + element.id + '" value="' + element.name + '" >';
             temp += t_tt;
             var t_tt = '<input type="hidden" id="G' + element.id + '" value="' + element.chart_image + '" >';
@@ -281,7 +392,7 @@ $.ajax({
             //element.name = '"'+element.name+'"';
             var t_url = '"' + api_url1 + element.id + '?name=' + element.name + '"';
 
-            temp += '<tr style="cursor: pointer;" onclick="test(' + element.id + ');"> <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;<span class="highlgt">' + element.name + '</span></td><td id="' + element.id + '">' + element.price + '</td><td class="numeric">' + element.percent_change_24h + '</td><td class="numeric">' + element.volume_24h + '</td><td class="numeric">' + element.market_cap + '</td><td class="numeric"> <img height="50" style="border: 1px solid #eee;" width="150" src="' + element.chart_image + '"> </td></tr>';
+            temp += '<tr style="cursor: pointer;" onclick="test(' + element.id + ');"> <td> <img src="' + element.image_url + '" height="16" width="16">&nbsp;&nbsp;<span class="highlgt">' + element.name + '</span></td><td id="' + element.id + '">' + element.price + '</td><td class="numeric">' + element.percent_change_24h + '</td><td class="numeric">' + element.volume_24h + '</td><td class="numeric">$ ' + element.market_cap + '</td><td class="numeric"> <img height="50" style="border: 1px solid #eee;" width="150" src="' + element.chart_image + '"> </td></tr>';
 
 
         });
