ca028697ba7d8bbe9cfcb1627913f686e6836136
Victor <ingsanchez423@gmail.com>
2019-10-21 11:58:03 -0400

    sort fix bug

--- resources/views/new_index.blade.php
+++ resources/views/new_index.blade.php
@@ -290,7 +290,7 @@
                                         </span> --}}
                                     </td>
                                     <td class="tbl-col-md change-period clickable-coin-td text-right" >
-                                       <span id="score" class="tbl-price pr-change delta-859 perc_none p-0"> {{number_format(($item->score_1d + $item->score_7d + $item->score_14d + $item->score_30d + $item->score_90d), 6, '.', ',')}} </span>
+                                       <span id="score" class="tbl-price pr-change delta-859 perc_none p-0"> {{number_format(($item->score_1d + $item->score_7d + $item->score_14d + $item->score_30d+$item->score_90d), 6, '.', ',')}} </span>
                                     </td>
                                 </tr>
                             @endforeach
